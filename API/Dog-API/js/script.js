49bc47a727dec4a06feaa3df0bb38c3d78464328
dipalihadiya <145538993+dipalihadiya@users.noreply.github.com>
2024-07-21 09:04:08 +0530

    Add files via upload

---
+++ API/Dog-API/js/script.js
@@ -0,0 +1,72 @@
+let printlist = document.getElementById('printlist');
+let dogImage = document.getElementById('images');
+const dogAPI = () => {
+      fetch('https://dog.ceo/api/breeds/list/all').then((res) => {
+            return res.json();
+      }).then((list) => {
+            let breedlist = list.message;
+            console.log(breedlist);
+
+            for (const breed in breedlist) {
+                  
+                  if(breedlist[breed].length == 0){
+                        
+                        console.log('breed[key]',breedlist[breed]);
+
+                        printlist.innerHTML += `<li onclick="return listImage('${breed}')">${breed}</li>`;
+
+                  }else{
+
+                        console.log(breedlist[breed]);
+
+                        orderlist = '<ol>';
+
+                        for (const subbreed in breedlist[breed]) {
+                              orderlist += `<li onclick="return listImage('${breed}')">${breedlist[breed][subbreed]}</li>`
+                        }
+
+                        orderlist += '</ol>';
+
+                        printlist.innerHTML += `<li>${breed} ${orderlist}</li>`
+
+
+                  }
+            }
+
+      }).catch((err) => {
+
+            console.log(err);
+
+      })
+}
+
+dogAPI();
+
+const listImage = (breed) => {
+
+      console.log("clickkk");
+      fetch(`https://dog.ceo/api/breed/${breed}/images`).then((res) => {
+
+            return res.json();
+      }).then((list) => {
+
+            let breedImage = list.message;
+
+            console.log('breed' ,breedImage);
+
+            dogImage.innerHTML = " ";
+
+            breedImage.forEach((img) => {
+                  dogImage.innerHTML += `<div class="col-3 ">
+                              
+                  <img src="${img}" alt="img" />
+                        </div>`
+            })
+
+      }).catch((err) => {
+
+            console.log('err',err);
+      })
+
+
+}
